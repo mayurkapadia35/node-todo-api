0a074a196c0041b5e6f3c678d3a598f97b040c82
Mayur Kapadia <lanetteam.mayur35@gmail.com>
2018-02-05 14:44:55 +0530

    hashing password commiut

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-    id: 10
-};
+var password='123abc';
 
-var token=jwt.sign(data,'123abc');
-console.log(token);
+bcrypt.genSalt(10,(err,salt)=>{
+   bcrypt.hash(password,salt,(err,hash)=>{
+      //console.log(hash);
+   });
+});
 
-var decoded=jwt.verify(token,'123ab');
-console.log('decodedsdsf',decoded);
+
+var hashedPassword='$2a$10$Xnq5O0FjJ0ageSTLnIcVleqRiS6cI3Bh1jvXCetjKz.chSBh9YFSi';
+bcrypt.compare('123!',hashedPassword,(err,res)=>{
+    console.log(res);
+});
+// var data={
+//     id: 10
+// };
+//
+// var token=jwt.sign(data,'123abc');
+// console.log(token);
+//
+// var decoded=jwt.verify(token,'123ab');
+// console.log('decodedsdsf',decoded);
 
 // var message='I am a user number 3';
 // var hash=SHA256(message).toString();
